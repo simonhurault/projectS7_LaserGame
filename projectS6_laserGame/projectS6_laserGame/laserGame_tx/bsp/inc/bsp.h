84550fcef38f108db901f45021aa4e7869c2aef9
Simon HURAULT <simon.hurault@isim.intra>
2021-01-28 17:19:12 +0100

    push button control

--- projectS6_laserGame/projectS6_laserGame/laserGame_tx/bsp/inc/bsp.h
+++ projectS6_laserGame/projectS6_laserGame/laserGame_tx/bsp/inc/bsp.h
@@ -10,4 +10,11 @@
 
 void BSP_TIMER_PWM_Init(void);
 
+void BSP_Console_Init(void);
+
+void BSP_PB_Init(void);
+uint8_t BSP_PB_GetState(void);
+
+void BSP_NVIC_Init(void);
+
 #endif /* BSP_INC_BSP_H_ */
